d6340c2af9053979726353407c16f140e14d6eff
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-07-08 12:54:07 -0300

    ajeitando

--- desafios react/src/App.jsx
+++ desafios react/src/App.jsx
@@ -2,6 +2,9 @@
 import './App.css'
 import RateEstrela from './desafios/rate-em-estrela/desafio1'
 import QRCodeGerador from './desafios/QR-gerador/Gerador'
+import CorAleatoria from './desafios/Cor-aleatoria/Cor'
+import JogoDaVelha from './desafios/jogo/jogo-da-velha'
+
 function App() {
   
 
@@ -11,8 +14,13 @@ function App() {
     {/*<h2>Rate em Estrela</h2> */}
    {/* <RateEstrela noEstrela={10}/> */}
 
-    <h2>Gerador de QR code</h2>
-    <QRCodeGerador />
+    {/* <h2>Gerador de QR code</h2>
+    <QRCodeGerador /> */}
+    {/* <h2>Criador de cor aleatoria</h2>
+    <CorAleatoria /> */}
+    
+    {/* <h2> Jogo da velha</h2>
+    <JogoDaVelha/> */}
     </>
   )
 }
